ea9faa40fa79bcac18f60df10a216d39de183c49
sophiag16 <sophia.g16@gmail.com>
2015-09-19 13:34:38 +0530

    added specs to parser

--- test/com/tw/baseline5/ParserTest.java
+++ test/com/tw/baseline5/ParserTest.java
@@ -15,4 +15,34 @@ public class ParserTest {
 
         assertEquals("X", splitInput[0][0]);
     }
+
+    @Test
+    public void shouldReturnArrayForString01() {
+        String input = "XX\n" +
+                "XX";
+        String[][] splitInput = {{"X", "X"}, {"X", "X"}};
+        Parser parser =  new Parser();
+
+        assertEquals("X", splitInput[0][1]);
+    }
+
+    @Test
+    public void shouldReturnArrayForString10() {
+        String input = "XX\n" +
+                "XX";
+        String[][] splitInput = {{"X", "X"}, {"X", "X"}};
+        Parser parser =  new Parser();
+
+        assertEquals("X", splitInput[1][0]);
+    }
+
+    @Test
+    public void shouldReturnArrayForString11() {
+        String input = "XX\n" +
+                "XX";
+        String[][] splitInput = {{"X", "X"}, {"X", "X"}};
+        Parser parser =  new Parser();
+
+        assertEquals("X", splitInput[1][1]);
+    }
 }
